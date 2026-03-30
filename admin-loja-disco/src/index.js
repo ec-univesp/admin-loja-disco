cad7fca530b5f48b64d5df4bab8a32b0b5ae47bd
Emerson Silva <emerson_jdss@hotmail.com>
2026-03-29 21:50:00 -0300

    data

---
+++ admin-loja-disco/src/index.js
@@ -0,0 +1,28 @@
+function Main() {
+    buscarArtista(nomeDoArtista) {
+        return nomeDoArtista;
+    }
+
+   async buscarArtista(response, request) {
+
+        1
+        const valorResposta = request.body.nomeArtista
+        2
+        await vouNoBancoDeDados(valorResposta);
+        3
+        return valorResposta; true
+
+
+    }
+
+}
+
+Main.buscarArtista("Beyoncé");
+
+const frutas = ["maca", "laranja"]
+
+[
+    {
+
+    }
+]
