47a3b2dac9c18f063767797b1fff5d4dc4226b48
Emerson Silva <emerson_jdss@hotmail.com>
2026-04-04 16:24:26 -0300

    adjust jsvectormap

--- jsvectormap.d.ts
+++ jsvectormap.d.ts
@@ -1,4 +1,11 @@
 declare module 'jsvectormap' {
-    const jsVectorMap: any;
+    export interface jsVectorMapOptions {
+        [key: string]: unknown;
+    }
+
+    export class jsVectorMap {
+        constructor(options: jsVectorMapOptions);
+    }
+
     export default jsVectorMap;
 }
