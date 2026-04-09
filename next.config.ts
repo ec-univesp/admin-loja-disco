fe748e6f560a5eec2eb8d37fa6cafec5bd0c55e6
Emerson Silva <emerson_jdss@hotmail.com>
2026-04-09 00:12:00 -0300

    adjust configs on nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
