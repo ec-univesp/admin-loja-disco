0647e99a966a0670dfec092200c08e744ec0e398
Emerson Silva <emerson_jdss@hotmail.com>
2026-04-25 23:12:43 -0300

    chore(cleanup): remove forms mortos, sidebar widget vazio e inlina InputField em ControlledInput

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -13,7 +13,6 @@ import {
   HorizontaLDots,
   PaperPlaneIcon,
 } from '../icons/index';
-import SidebarWidget from './SidebarWidget';
 
 type NavItem = {
   name: string;
@@ -251,7 +250,6 @@ const AppSidebar: React.FC = () => {
             </div>
           </div>
         </nav>
-        {isExpanded || isHovered || isMobileOpen ? <SidebarWidget /> : null}
       </div>
     </aside>
   );
