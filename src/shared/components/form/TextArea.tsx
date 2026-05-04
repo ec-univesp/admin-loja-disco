0e8cb39aecf11a65e7a922785843f67b7c8c38a4
Emerson Silva <emerson_jdss@hotmail.com>
2026-05-03 21:34:59 -0300

    traduz textos em ingles dos componentes compartilhados

--- src/shared/components/form/TextArea.tsx
+++ src/shared/components/form/TextArea.tsx
@@ -12,7 +12,7 @@ interface TextareaProps {
 }
 
 const TextArea: React.FC<TextareaProps> = ({
-  placeholder = 'Enter your message', // Default placeholder
+  placeholder = 'Digite sua mensagem',
   rows = 3, // Default number of rows
   value = '', // Default value
   onChange, // Callback for changes
